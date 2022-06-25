5fed3a1659db879ed82c962a0e92f456aa6ab384
MarcoPacciani <marcopac007@gmail.com>
2022-06-25 03:37:46 +0200

    logica per prendere la durata dei video

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,4 +12,8 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onMetadata(e:any, video:any) {
+    console.log('metadata: ', e);
+    console.log('duration: ', video.duration);
+  }
 }
